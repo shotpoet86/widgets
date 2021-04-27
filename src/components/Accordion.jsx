1ca6b25f7c5229cbb9b95046d7ccb92988c4bd80
shotpoet86 <curlybracemaster@gmail.com>
2021-04-27 15:30:58 -0400

    added useState hook.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
   /*helper function*/
   const onTitleClick = (index) => {
-    console.log('title clicked', index);
+    setActiveIndex(index);
   };
-  /*renders items by mapping through items array*/
+
+  /*renders items by mapping through items array and displays on user screen*/
   const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        {/*listens for onClick and calls onTitleClick function passing each elements index from items array*/}
+        {/*listens for onClick from web api and calls onTitleClick function passing each elements index from items array*/}
         <div className="title active" onClick={() => onTitleClick(index)}>
+          {/*displays item title*/}
           <i className="dropdown icon"></i>
           {item.title}
         </div>
+        {/*dislpays item content*/}
         <div className="content active">
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
