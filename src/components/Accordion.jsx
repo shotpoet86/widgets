9de87e9189a85140da86138b1f8f02977037d691
shotpoet86 <curlybracemaster@gmail.com>
2021-04-28 12:19:41 -0400

    added dropdown functionality to widgets.

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -10,28 +10,26 @@ const Accordion = ({ items }) => {
 
   /*renders items by mapping through items array and displays on user screen*/
   const renderedItems = items.map((item, index) => {
+    /*if index is equal to activeIndex, selected index will dropDown and display, otherwise will stay closed*/
+    const active = index === activeIndex ? 'active' : '';
+    console.log(active + index);
     return (
       <React.Fragment key={item.title}>
         {/*listens for onClick from web api and calls onTitleClick function passing each elements index from items array*/}
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           {/*displays item title*/}
           <i className="dropdown icon"></i>
           {item.title}
         </div>
         {/*dislpays item content*/}
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
