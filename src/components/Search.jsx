d249b555c9005d2024c048558662c77723649446
shotpoet86 <curlybracemaster@gmail.com>
2021-05-06 12:34:18 -0400

    return data from wiki api.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('JavaScript');
+  const [results, setResults] = useState([]);
+  console.log(results);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <div className="ui input ui form">
@@ -10,10 +43,11 @@ const Search = () => {
             className="input"
             type="text"
             value={term}
-            onChange={(e) => setTerm(e.target.value)}
+            onChange={(event) => setTerm(event.target.value)}
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
